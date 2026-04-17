703a639cd86bf37f5aa27bc635b64afb0f90efaa
ovsok <ovsokolov@gmail.com>
2026-04-17 11:00:45 -0400

    Revert "Fix autoreconnect enum order after rollback"
    This reverts commit b2dc5c91c9f33f3d80a11d143281d055782648bd.

--- headset_hfp_hf/mtb-example-btsdk-audio-headset-standalone/wiced_app.c
+++ headset_hfp_hf/mtb-example-btsdk-audio-headset-standalone/wiced_app.c
@@ -83,13 +83,6 @@ static wiced_bt_buffer_pool_t* watch_app_pool_small = NULL;
 
 #define WICED_HS_EIR_BUF_MAX_SIZE 264
 
-typedef enum
-{
-    HF_AUTORECONNECT_IDLE = 0,
-    HF_AUTORECONNECT_HFP,
-    HF_AUTORECONNECT_AVRCP,
-    HF_AUTORECONNECT_A2DP,
-} hf_autoreconnect_stage_t;
 
 static void write_eir(void);
 static wiced_result_t btm_enabled_event_handler(wiced_bt_dev_enabled_t *event_data);
@@ -122,6 +115,14 @@ static app_identity_random_mapping_t addr_mapping[ADDR_MAPPING_MAX_COUNT] = {0};
 static wiced_timer_t delayed_bond_dump_timer;
 static wiced_timer_t hf_autoreconnect_timer;
 static wiced_bt_device_address_t hf_autoreconnect_bda = {0};
+
+typedef enum
+{
+    HF_AUTORECONNECT_IDLE = 0,
+    HF_AUTORECONNECT_HFP,
+    HF_AUTORECONNECT_AVRCP,
+    HF_AUTORECONNECT_A2DP,
+} hf_autoreconnect_stage_t;
 
 static hf_autoreconnect_stage_t hf_autoreconnect_stage = HF_AUTORECONNECT_IDLE;
 
